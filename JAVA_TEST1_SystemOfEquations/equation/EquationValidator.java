9cd573c275b314cfdf9856f1c1738eecc923292c
DeonBugire <122877911+DeonBugire@users.noreply.github.com>
2024-07-10 15:33:22 +0300

    Update EquationValidator.java

--- JAVA_TEST1_SystemOfEquations/equation/EquationValidator.java
+++ JAVA_TEST1_SystemOfEquations/equation/EquationValidator.java
@@ -8,11 +8,7 @@ public class EquationValidator {
         "([-+]?\\d*\\.?\\d*)x\\s*([-+]?\\d*\\.?\\d*)y\\s*([-+]?\\d*\\.?\\d*)z\\s*([-+]?\\d*\\.?\\d*)\\s*=\\s*([-+]?\\d*\\.?\\d*)"
     );
 
-    /**
-     * Проверяет, является ли строка системой уравнений из четырех линейных уравнений.
-     * @param content строка, содержащая систему уравнений.
-     * @return true, если система уравнений валидна, false иначе.
-     */
+
     public boolean isValidEquationSystem(String content) {
         String[] lines = content.split("\\R");
         if (lines.length != 4) {
